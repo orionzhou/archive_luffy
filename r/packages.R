11db843e0268de94b1d727148b85583b26f76e05
orionzhou <zhoupenggeni@gmail.com>
2012-12-19 11:26:49 -0600

    add ignored directories

---
+++ r/packages.R
@@ -0,0 +1,17 @@
+#source("http://www.bioconductor.org/biocLite.R")
+require(ggplot2)
+require(data.table)
+#require(Hmisc)
+#require(ape)
+#require(gplots)
+#require(VennDiagram)
+
+#bioconductor packages
+#require(ctc)
+#require(limma)
+#require(Heatplus)
+#require(Rsamtools)
+#require(GenomicRanges)
+#require(GenomicFeatures)
+
+

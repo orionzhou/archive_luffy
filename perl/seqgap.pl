51f32617afdeda93290e32e7f99a6b522cc130cc
orionzhou <zhoupenggeni@gmail.com>
2014-06-14 10:37:00 -0500

    k

--- perl/seqgap.pl
+++ perl/seqgap.pl
@@ -6,17 +6,31 @@
   
 =head1 NAME
   
-  seqgap.pl - report gap ('N's) locations in an input fasta file
+  seqgap.pl - report gap ('N's) locations in fasta sequences
 
 =head1 SYNOPSIS
   
-  seqgap.pl [-help] [-min minimum-len] [-in input-file] [-out output-file] 
+  seqgap.pl [-help] [-min minimum-gap] [-in input-file] [-out output-file] 
 
   Options:
     -h (--help)   brief help message
     -i (--in)     input (Fasta) file
     -o (--out)    output (BED) file
     -m (--min)    minimum length of gaps to report (default: 10)
+
+=head1 DESCRIPTION
+  
+  For each gap (consecutive 'N's) in input sequences, report its locations
+  in tabular-separated  BED format with three fields: 'sequence-ID', 
+  'start' and 'end'.
+  Example output:
+    chr1   5      50
+    chr1   100    131
+    chr5   56     100
+
+=head1 AUTHOR
+  
+  Peng Zhou - University of Minnesota
 
 =cut
   
@@ -32,7 +46,7 @@ use Bio::SeqIO;
 my ($fi, $fo) = ('') x 2;
 my $fhi;
 my $fho;
-my $len_min = 1;
+my $len_min = 10;
 my $help_flag;
 
 #--------------------------------- MAIN -----------------------------------#
@@ -45,28 +59,37 @@ GetOptions(
 pod2usage(1) if $help_flag;
 pod2usage(2) if !$fi || !$fo;
 
+# read from stdin or opens an input file handler
 if ($fi eq "stdin" || $fi eq "-") {
   $fhi = \*STDIN;
 } else {
   open ($fhi, $fi) || die "Can't open file $fi: $!\n";
 }
 
+# write to stdout or opens an output file handler
 if ($fo eq "stdout" || $fo eq "-") {
   $fho = \*STDOUT;
 } else {
   open ($fho, ">$fo") || die "Can't open file $fo for writing: $!\n";
 }
 
+# initialize a Bio::SeqIO object using input handler
 my $seqHI = Bio::SeqIO->new(-fh=>$fhi, -format=>'fasta');
-open($fho, ">$fo") or die "cannot open $fo for writing\n";
-#print $fho join("\t", qw/id beg end len/)."\n";
 
+# iterate through input sequences
 while(my $seqO = $seqHI->next_seq()) {
   my ($id, $seq, $seqlen) = ($seqO->id, $seqO->seq, $seqO->length);
+
+# using regexp to find gaps (consecutive 'N's in a string),
+# determine their locations and lengths, filter, and output
   while($seq =~ /N+/ig) {
     my ($beg, $end) = ($-[0]+1, $+[0]);
     my $len = $end - $beg + 1;
-    next if $len < $len_min;
+
+# don't report short gaps
+    $len >= $len_min || next;
+
+# note that BED format use 0-based start coordinates
     print $fho join("\t", $id, $beg - 1, $end)."\n";
   }
 }
--- perl/seqtile.pl
+++ perl/seqtile.pl
@@ -45,49 +45,49 @@ my ($size, $step) = (60, 5);
 my ($fhi, $fho);
 my $help_flag;
 
-#----------------------------------- MAIN -----------------------------------#
+#--------------------------------- MAIN -----------------------------------#
 GetOptions(
-    "help|h"   => \$help_flag,
-    "in|i=s"   => \$fi,
-    "out|o=s"  => \$fo,
-    "step|t=i" => \$step,
-    "size|z=i" => \$size,
+  "help|h"   => \$help_flag,
+  "in|i=s"   => \$fi,
+  "out|o=s"  => \$fo,
+  "step|t=i" => \$step,
+  "size|z=i" => \$size,
 ) or pod2usage(2);
 pod2usage(1) if $help_flag;
 
 if ($fi eq "" || $fi eq "stdin" || $fi eq "-") {
-    $fhi = \*STDIN;
+  $fhi = \*STDIN;
 } else {
-    open ($fhi, $fi) || die "Can't open file $fi: $!\n";
+  open ($fhi, $fi) || die "Can't open file $fi: $!\n";
 }
 
 if ($fo eq "" || $fo eq "stdout" || $fo eq "-") {
-    $fho = \*STDOUT;
+  $fho = \*STDOUT;
 } else {
-    open ($fho, ">$fo") || die "Can't open file $fo for writing: $!\n";
+  open ($fho, ">$fo") || die "Can't open file $fo for writing: $!\n";
 }
 
 my $seqHI = Bio::SeqIO->new(-fh=>$fhi, -format=>'fasta');
 my $cnt = 0;
 while(my $seqO = $seqHI->next_seq()) {
-    my ($id, $len, $seq) = ($seqO->id, $seqO->length, $seqO->seq);
-    my ($beg, $end) = (1, $len);
-    if($id =~ /^(\w+)\:(\d+)\-(\d+)$/) {
-        ($id, $beg, $end) = ($1, $2, $3);
-    }
-    
-    my $n_win = int(($end-$beg+1-$size)/$step) + 1;
-    next if $n_win < 1;
+  my ($id, $len, $seq) = ($seqO->id, $seqO->length, $seqO->seq);
+  my ($beg, $end) = (1, $len);
+  if($id =~ /^(\w+)\:(\d+)\-(\d+)$/) {
+    ($id, $beg, $end) = ($1, $2, $3);
+  }
+  
+  my $n_win = int(($end-$beg+1-$size)/$step) + 1;
+  next if $n_win < 1;
 
-    my @wins;
-    for my $i (0..$n_win-1) {
-        my $beg1 = $beg + $step * $i;
-        my $end1 = $beg + $step * $i + $size - 1;
-        $end1 <= $end || die "$id:[$beg-$end] range error: [$beg1-$end1]\n";
-        my $seq1 = substr($seq, $step*$i, $size);
-        print $fho ">$id:$beg1-$end1\n";
-        print $fho "$seq1\n";
-    }
+  my @wins;
+  for my $i (0..$n_win-1) {
+    my $beg1 = $beg + $step * $i;
+    my $end1 = $beg + $step * $i + $size - 1;
+    $end1 <= $end || die "$id:[$beg-$end] range error: [$beg1-$end1]\n";
+    my $seq1 = substr($seq, $step*$i, $size);
+    print $fho ">$id:$beg1-$end1\n";
+    print $fho "$seq1\n";
+  }
 }
 $seqHI->close();
 close $fho;

13e111c361eda99189c58925e74131d945b12e9e
orionzhou <zhoupenggeni@gmail.com>
2018-06-11 16:45:48 -0500

    normal commit

--- r/enrich.R
+++ r/enrich.R
@@ -1,59 +1,69 @@
-require(plyr)
-require(tidyr)
-require(dplyr)
+require(tidyverse)
 
+if (FALSE) {
 fg = '/home/springer/zhoux379/data/genome/Zmays_v4/61.interpro/15.tsv'
 tg = read.table(fg, sep = "\t", as.is = T, header = F, quote = '')
 colnames(tg) = c("gid", "goid")
 
 fd = '/home/springer/zhoux379/data/genome/Zmays_v4/61.interpro/16.go.tsv'
-td = read.table(fd, sep = "\t", header = T, as.is = T, quote = '')
+#td = read.table(fd, sep = "\t", header = T, as.is = T, quote = '')
 
 fi = '/home/springer/zhoux379/data/genome/Zmays_v4/61.interpro/gids.txt'
 ti = read.table(fi, as.is = T)
 gids.all = ti$V1
-
-
-go_enrich <- function(gids, gids.all. = gids.all, tg. = tg, td. = td) {
-	tgs = tg %>% 
-		group_by(goid) %>% 
-		summarise(hitInPop = n())
-
-	sampleSize = length(gids)
-
-	tz = tg[tg$gid %in% gids,] %>%
-		group_by(goid) %>% 
-		summarise(hitInSample = n())
-
-	tw = merge(tz, tgs, by = 'goid')
-	tw = tw %>% 
-		mutate(sampleSize = length(gids), popSize = length(gids.all), 
-			pval.raw = phyper(hitInSample-1, hitInPop, popSize-hitInPop, sampleSize, lower.tail = F),
-			pval.adj = p.adjust(pval.raw, method = "BH")) %>%
-		filter(pval.raw < 0.05) %>%
-		arrange(pval.adj) %>%
-		transmute(goid = goid, ratioInSample = sprintf("%d/%d", hitInSample, sampleSize),
-			ratioInPop = sprintf("%d/%d", hitInPop, popSize),
-			pval.raw = pval.raw, pval.adj = pval.adj)
-
-	to = merge(tw, td, by = 'goid')
-	to %>% arrange(pval.adj, pval.raw)
 }
+
+fgo = '/home/springer/zhoux379/data/genome/B73/GO/10.tsv'
+tgo = read_tsv(fgo)
+unique(tgo$ctag)
+gids_all = unique(tgo$gid)
+tgo_ipr = tgo %>% filter(ctag == 'Interproscan5') %>% select(-ctag)
+tgo_uni = tgo %>% filter(ctag == 'uniprot.plants') %>% select(-ctag)
+tgo_ath = tgo %>% filter(ctag == 'arabidopsis') %>% select(-ctag)
+tgo_arg = tgo %>% filter(ctag == 'argot2.5') %>% select(-ctag)
+
+go_enrich <- function(gids, tg) {
+    tgn = tg %>% distinct(goid, goname, gotype, level)
+    tgs = tg %>% count(goid) %>% transmute(goid=goid, hitInPop = n)
+
+    gids_all = tg %>% distinct(gid) %>% pull(gid)
+    gids = unique(gids)
+    gids = gids[gids %in% gids_all]
+    sampleSize = length(gids)
+
+    tz = tg %>% filter(gid %in% gids) %>%
+        count(goid) %>% 
+        transmute(goid = goid, hitInSample = n)
+
+    tw = tz %>% inner_join(tgs, by = 'goid') %>% 
+        mutate(sampleSize = length(gids), popSize = length(gids_all), 
+            pval.raw = phyper(hitInSample-1, hitInPop, popSize-hitInPop, sampleSize, lower.tail = F),
+            pval.adj = p.adjust(pval.raw, method = "BH")) %>%
+        filter(pval.raw < 0.05) %>%
+        arrange(pval.adj) %>%
+        transmute(goid = goid, ratioInSample = sprintf("%d/%d", hitInSample, sampleSize),
+            ratioInPop = sprintf("%d/%d", hitInPop, popSize),
+            pval.raw = pval.raw, pval.adj = pval.adj)
+
+    tw %>% left_join(tgn, by = 'goid') %>% arrange(pval.adj, pval.raw)
+}
+
+go_enrich_all <- function(gids, tgo. = tgo, srcs = c("uniprot.plants", "arabidopsis", "Interproscan5")) {
+    to = tibble()
+    for (src in srcs) {
+        tgoc = tgo %>% filter(ctag == src) %>% select(-ctag)
+        to1 = go_enrich(gids, tgoc) %>% 
+            filter(pval.adj <= 0.05) %>%
+            mutate(source = src) %>%
+            select(source, goid, ratioInSample, ratioInPop, pval.adj, gotype, goname)
+        to = rbind(to, to1)
+    }
+    to
+}
+
 
 #fisher.test(matrix(c(hitInSample, hitInPop-hitInSample, sampleSize-hitInSample, failInPop-sampleSize +hitInSample), 2, 2), alternative='two.sided')
 
 
-
-dirw = '/home/springer/zhoux379/data/misc2/peter_chg_go'
-
-fi = file.path(dirw, '01.gid.txt')
-ti = read.table(fi, as.is = T)
-gids = unique(ti$V1)
-
-fi = file.path(dirw, '02.gids.bg.txt')
-ti = read.table(fi, as.is = T)
-gids = unique(ti$V1)
-
-to = go_enrich(gids)
 
 
